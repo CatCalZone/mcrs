d5872865d4802eb159503866922239ed94c575f6
aderenbach@gmail.com <aderenbach@gmail.com>
2015-05-15 09:55:12 +0200

    added zuul

--- applications/appointmentGateway/src/main/java/com/zuehlke/mcrs/gateway/aws/AWSFactory.java
+++ applications/appointmentGateway/src/main/java/com/zuehlke/mcrs/gateway/aws/AWSFactory.java
@@ -19,29 +19,36 @@ public class AWSFactory {
     @Value("${incomingAppointmentQueue}")
     private String incomingSMSQueue;
 
+    @Value("${sendToSqs:true}")
+    private boolean sendToSqs;
 
     public AmazonSQS createSQS() {
 
-        AWSCredentials credentials = null;
-
         try {
 
-            credentials = new BasicAWSCredentials("x", "x");
+            AWSCredentials credentials = null;
+
+            try {
+
+                credentials = new BasicAWSCredentials("x", "x");
+
+            } catch (Exception e) {
+                throw new AmazonClientException(
+                        "Cannot load the credentials from the credential profiles file. " +
+                                "Please make sure that your credentials file is at the correct " +
+                                "location (~/.aws/credentials), and is in valid format.",
+                        e);
+            }
+
+            AmazonSQS sqs = new AmazonSQSClient(credentials);
+            sqs.setEndpoint("http://localhost:9324");
+            Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+            // sqs.setRegion(usWest2);
+            sqs.createQueue(incomingSMSQueue);
+            return sqs;
 
         } catch (Exception e) {
-            throw new AmazonClientException(
-                    "Cannot load the credentials from the credential profiles file. " +
-                            "Please make sure that your credentials file is at the correct " +
-                            "location (~/.aws/credentials), and is in valid format.",
-                    e);
+            return null; //TODO actually application start fails if no SQS is running.
         }
-
-        AmazonSQS sqs = new AmazonSQSClient(credentials);
-        sqs.setEndpoint("http://localhost:9324");
-        Region usWest2 = Region.getRegion(Regions.US_WEST_2);
-       // sqs.setRegion(usWest2);
-        sqs.createQueue(incomingSMSQueue);
-        return sqs;
-
     }
 }
---
+++ applications/mcrs-zuul/src/main/java/com/zuehlke/calcatzone/zuul/ZuulApplication.java
@@ -0,0 +1,18 @@
+package com.zuehlke.calcatzone.zuul;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by kinggrass on 13.05.15.
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulApplication.class).web(true).run(args);
+    }
+
+}
