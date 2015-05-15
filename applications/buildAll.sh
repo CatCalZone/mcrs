2961dbbaa814b7de8b0964c9a2b669418422c250
Alexander Derenbach <dea@zuehlke.com>
2015-05-15 11:56:02 +0200

    refactored folders

--- applications/buildAll.sh
+++ applications/buildAll.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
 
+BASE_DIR="`dirname \"$0\"`"      
+BASE_DIR="`( cd \"$BASE_DIR\" && pwd )`"  
+if [ -z "$BASE_DIR" ] ; then
+  exit 1  # fail
+fi
+echo "$BASE_DIR"
+
+
 echo "building mcrs-eureka"
-cd mcrs-eureka && ./gradlew clean build && cd ..
+cd supportingServices/mcrs-eureka && ./gradlew clean build 
+cd $BASE_DIR
+
 echo "building mcrs-config"
-cd mcrs-config && ./gradlew clean build && cd ..
+cd supportingServices/mcrs-config && ./gradlew clean build 
+cd $BASE_DIR
+
+echo "building mcrs-zuul"
+cd supportingServices/mcrs-config && ./gradlew clean build 
+cd $BASE_DIR
+
 echo "building appointmentGateway"
-cd appointmentGateway && ./gradlew clean build && cd ..
+cd supportingServices/appointmentGateway && ./gradlew clean build 
+cd $BASE_DIR
+
 echo "building theProducer"
-cd theProducer && ./gradlew clean build && cd ..
+cd theProducer && ./gradlew clean build 
+cd $BASE_DIR
